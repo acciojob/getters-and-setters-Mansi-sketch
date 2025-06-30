57dc219d8c92168fb671119acada1f91f4ef2622
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-01 03:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
 
-class Student extends Person {}
+	get name(){
+		return this.name;
+	}
 
-class Teacher extends Person {}
+	set age(age){
+		this.age = age;
+	}
+}
+
+class Student extends Person {
+	study(){
+		console.log(this.name + " is studying");
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(this.name + " is teaching");
+	}
+}
+
+const person = new Person(name, age);
+console.log(person.name);
+
+person.age = 30; 
+console.log(person.age); 
+
+onst student = new Student("Alice", 22);
+student.study();
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
